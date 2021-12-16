aa13da978d984017450b257cd3f910e8a85fad6b
Ahmet Uysal <auysal16@ku.edu.tr>
2021-12-16 03:07:45 +0300

    feat: proxy mixpanel js sdk files

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,40 @@
-import { Controller, Get, Req, Res, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  StreamableFile,
+  Header,
+  Req,
+  Res,
+  Post,
+} from '@nestjs/common';
+import { createReadStream } from 'fs';
+import { join } from 'path';
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { MixpanelService } from './mixpanel/mixpanel.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly mixpanelService: MixpanelService) {}
+
+  @Get('lib.js')
+  @Header('Content-Type', 'text/javascript')
+  @Header('Content-Disposition', 'inline')
+  mixpanelJsLib(): StreamableFile {
+    const file = createReadStream(
+      join(process.cwd(), 'lib/mixpanel/mixpanel-2-latest.js'),
+    );
+    return new StreamableFile(file);
+  }
+
+  @Get('lib.min.js')
+  @Header('Content-Type', 'text/javascript')
+  @Header('Content-Disposition', 'inline')
+  mixpanelJsLibMinified(): StreamableFile {
+    const file = createReadStream(
+      join(process.cwd(), 'lib/mixpanel/mixpanel-2-latest.min.js'),
+    );
+    return new StreamableFile(file);
+  }
 
   @Get('*')
   mixpanelApiGetRequest(
